6696381672639e79404be47f540c097f8ad74003
Marc Lacroix <marcus_lacroix@yahoo.fr>
2018-12-11 10:50:17 +0100

    Fix deprecated dependency

--- src/init.php
+++ src/init.php
@@ -16,7 +16,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Enqueue Gutenberg block assets for both frontend + backend.
  *
- * `wp-blocks`: includes block type registration and related functions.
+ * `wp-block-library`: includes block type registration and related functions.
  *
  * @since 1.0.0
  */
@@ -25,7 +25,7 @@ function section_block_cgb_block_assets() {
 	wp_enqueue_style(
 		'section_block-cgb-style-css', // Handle.
 		plugins_url( 'dist/blocks.style.build.css', dirname( __FILE__ ) ), // Block style CSS.
-		array( 'wp-blocks' ) // Dependency to include the CSS after it.
+		array( 'wp-block-library' ) // Dependency to include the CSS after it.
 		// filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.style.build.css' ) // Version: filemtime — Gets file modification time.
 	);
 } // End function section_block_cgb_block_assets().
@@ -36,7 +36,7 @@ add_action( 'enqueue_block_assets', 'section_block_cgb_block_assets' );
 /**
  * Enqueue Gutenberg block assets for backend editor.
  *
- * `wp-blocks`: includes block type registration and related functions.
+ * `wp-block-library`: includes block type registration and related functions.
  * `wp-element`: includes the WordPress Element abstraction for describing the structure of your blocks.
  * `wp-i18n`: To internationalize the block's text.
  *
@@ -47,7 +47,7 @@ function section_block_cgb_editor_assets() {
 	wp_enqueue_script(
 		'section_block-cgb-block-js', // Handle.
 		plugins_url( '/dist/blocks.build.js', dirname( __FILE__ ) ), // Block.build.js: We register the block here. Built with Webpack.
-		array( 'wp-blocks', 'wp-i18n', 'wp-element' ), // Dependencies, defined above.
+		array( 'wp-block-library', 'wp-i18n', 'wp-element' ), // Dependencies, defined above.
 		// filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.build.js' ), // Version: filemtime — Gets file modification time.
 		true // Enqueue the script in the footer.
 	);
